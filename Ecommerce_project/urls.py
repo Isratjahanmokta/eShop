62cef9c72b4fef3295f5963b704fd2efa417c7e8
Israt Jahan~ <israt.mukta06@gmail.com>
2023-04-03 22:50:10 +0600

    Created homepage template

--- Ecommerce_project/urls.py
+++ Ecommerce_project/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from eshop import views
+from eshop.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('eshop.urls')),
     path('', include('authentication.urls')),
-    path('', views.index, name='home'),
+    path('', Home.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- eshop/views.py
+++ eshop/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Product, Category
 # Create your views here.
 def index(request):
     return render(request, 'base.html', context= {'title':'Home'})
+
+class Home(ListView):
+    context_object_name = 'products'
+    model = Product
+    queryset = Product.objects.all()
+    template_name = 'eshop/home.html'
+    
