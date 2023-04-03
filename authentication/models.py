051cf3843d9202bfcb70d451c74c6e9aa65acd64
Israt Jahan~ <israt.mukta06@gmail.com>
2023-04-03 13:53:32 +0600

    created profile model

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ObjectDoesNotExist
 
 #To automatically created one to one relation
 from django.db.models.signals import post_save
@@ -10,26 +11,34 @@ class CustomUser(AbstractUser):
     pass
     
     def __str__(self) -> str:
-        return self.email
+        return self.username
+    
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
-#     username = models.CharField(max_length=264, blank=True)
-#     full_name = models.CharField(max_length=264, blank=True)
-#     address = models.TextField(max_length=300, blank=True)
-#     city = models.CharField(max_length=40, blank=True)
-#     zipcode = models.CharField(max_length=10, blank=True)
-#     country = models.CharField(max_length=50, blank=True)
-#     phone = models.CharField(max_length=20, blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    username = models.CharField(max_length=264, blank=True)
+    full_name = models.CharField(max_length=264, blank=True)
+    address = models.TextField(max_length=300, blank=True)
+    city = models.CharField(max_length=40, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
     
-#     def __str__(self):
-#         return self.user +"'s Profile"
+    def __str__(self):
+        return self.user
     
-#     def is_fully_filled(self):
-#         fields_names = [f.name for f in self._meta.get_fields()]
+    def is_fully_filled(self):
+        fields_names = [f.name for f in self._meta.get_fields()]
         
-#         for field_name in fields_names:
-#             value = getattr(self. field_name)
-#             if value is None or value == '':
-#                 return False
-#         return True
+        for field_name in fields_names:
+            value = getattr(self. field_name)
+            if value is None or value == '':
+                return False
+        return True
+    
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
