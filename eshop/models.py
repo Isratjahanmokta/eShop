0295452cc4251f99660a5212192b98f9f55d026e
Israt Jahan~ <israt.mukta06@gmail.com>
2023-04-03 16:20:11 +0600

    created Product ,Category model + install pillow

--- eshop/models.py
+++ eshop/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+class Product(models.Model):
+    prod_img = models.ImageField(upload_to='products')
+    name = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    preview = models.TextField(max_length =200, verbose_name='Preview Text')
+    detail_text = models.TextField(max_length=1000, verbose_name='Description')
+    price = models.FloatField()
+    old_price = models.FloatField(default = 0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        ordering = ['-created_at']
