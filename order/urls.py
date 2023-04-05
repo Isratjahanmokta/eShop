e24b22657044e755e6242d14b2db831d44d11e5c
Israt Jahan~ <israt.mukta06@gmail.com>
2023-04-05 17:43:03 +0600

    Created add item to cart view

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from order import views
 
 app_name = 'order'
 
 urlpatterns = [
-    
+    path('add/<pk>',views.add_to_cart, name='add_to_cart'),
+ 
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from order.models import Cart, Order
+from eshop.models import Product
+from django.contrib import messages
 
 # Create your views here.
+
+@login_required
+def add_to_cart(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_items = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.order_item.filter(item=item).exists():
+            order_items[0].quantity += 1
+            order_items[0].save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect("eshop:home")
+        else:
+            order.order_item.add(order_items[0])
+            messages.info(request, "This item was added to your cart.")
+            return redirect("eshop:home")
+    else:
+        order = Order(user=request.user)
+        order.save()
+        order.order_item.add(order_items[0])
+        messages.info(request, "This item was added to your cart.")
+        return redirect("eshop:home")
+            
