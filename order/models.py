d9bf90e354b396f43549c7a897edc90365f592a5
Israt Jahan~ <israt.mukta06@gmail.com>
2023-04-07 16:47:28 +0600

    Created Billing Address model

--- order/models.py
+++ order/models.py
@@ -32,3 +32,25 @@ class Order(models.Model):
         for order_items in self.order_item.all():
             total += float(order_items.get_tot_price())
         return total   
+    
+class BillingAddress(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address = models.CharField(max_length=250, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.profile.username} billing address'
+    
+    def is_fully_filled(self):
+        field_names = [f.name for f in self._meta.get_fields()]
+        
+        for field_name in field_names:
+            value = getattr(self, field_name)
+            if value is None or value == '':
+                return False
+        return True
+    
+    class Meta:
+        verbose_name_plural = "Billing Addresses"
